0404c4f4e0bed273c6223f4c329d8ddacd4bf002
Jimmy Zhu <jimmy_zhu@epam.com>
2018-11-15 17:37:56 +0800

    Aircraft Carrier

---
+++ week-02/day-04/Aircraft Carrier/Aircraft Carrier/Aircraft.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aircraft_Carrier
+{
+    class Aircraft
+    {
+        internal string type { get; set; }
+        internal int max_ammo { get; set; }
+        internal int base_damage { get; set; }
+    }
+}
