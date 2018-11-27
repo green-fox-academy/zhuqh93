7b83df4759d34c24d453fbff119d706554da7794
Jimmy Zhu <jimmy_zhu@epam.com>
2018-11-27 17:48:10 +0800

    week04-day02-workshop

---
+++ week-04/day-02/day-02-workshop/day-02-workshop/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using day_02_workshop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace day_02_workshop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static List<BankAccount> charactersList = new List<BankAccount>()
+        {
+            new BankAccount("Nala", 1500, "Lion", false, true),
+            new BankAccount("Timon", 1000, "Meerkat", false, true),
+            new BankAccount("Simba", 2000, "Lion", true, true),
+            new BankAccount("Pumbaa", 1000, "Warthog", false, true),
+            new BankAccount("Scar", 2000, "Lion", false, false)
+        };
+
+        [HttpGet("/show")]
+        public IActionResult BankAccount()
+        {
+            return View(charactersList.Find(x => x.Name.Equals("Simba")));
+        }
+
+        [HttpGet("/string")]
+        public IActionResult ShowString()
+        {
+            return View();
+        }
+
+        [HttpGet("")]
+        public IActionResult ShowCharacters()
+        {
+            return View();
+        }
+
+        [HttpPost("")]
+        public IActionResult RaiseBalance(BankAccount bankAccount)
+        {
+            if (bankAccount.IsKing)
+            {
+                bankAccount.Balance += 100;
+            }
+            else
+            {
+                bankAccount.Balance += 10;
+            }
+
+            return RedirectToAction(nameof(ShowCharacters));
+        }
+    }
+}
---
+++ week-04/day-02/day-02-workshop/day-02-workshop/Models/BankAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace day_02_workshop.Models
+{
+    public class BankAccount
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string AnimalType { get; set; }
+        public bool IsKing { get; set; }
+        public bool GoodOrBad { get; set; }
+
+        public BankAccount(string name, double balance, string animalType, bool isKing, bool goodOrBad)
+        {
+            this.Name = name;
+            this.Balance = balance;
+            this.AnimalType = animalType;
+            this.IsKing = isKing;
+            this.GoodOrBad = goodOrBad;
+        }
+
+//        public void RaiseBalance()
+//        {
+//            if (this.IsKing)
+//            {
+//                this.Balance += 100;
+//            }
+//            else
+//            {
+//                this.Balance += 10;
+//            }
+//        }
+    }
+}
